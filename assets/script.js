be195ccec31aeef34313e50c2dc36a9f5e7300e1
Sonny Addison <sonny.m.addison@hotmail.com>
2022-06-09 23:05:46 -0700

    Trying to figure out API call

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,93 @@
-let searchInfoEl = document.getElementById('searchInfo');
-let pastSearchesEl = document.getElementById('pastSearches');
-let cityInputEl = document.getElementById('city');
-let stateInputEl = document.getElementById('state');
-let searchReturnEl = document.getElementById('return');
+var searchInfoEl = document.getElementById('searchInfo');
+var pastSearchesEl = document.getElementById('pastSearches');
+var cityInputEl = document.getElementById('city');
+/*var stateInputEl = document.getElementById('state');
+var countryInputEl = document.getElementById('country')*/
+var searchReturnEl = document.getElementById('return');
 
 /* Set API Key to Const so that no one can change it */
 const apiKey = '84e365eb6c0f563feed2e8ed0ff0400f'
 
 /* Getting search varibles listed out together */
-let queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + state + '&exclude=minute,hourly,&appid=' + apiKey;
+var queryURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + apiKey;
 
-let formSubmitHandler = function (event) {
+
+var formSubmitHandler = function (event) {
     event.preventDefault();
 
     var city = cityInputEl.value.trim();
-    var state = stateInputEl.value.trim();
+   /*var state = stateInputEl.value.trim();
+    var country = stateInputEl.value.trim();*/
 
-    if (city&&state) {
-        fetch(queryURL);
-        searchInfoEl.textContent ='';
+    if (city) {
+        newSearch(city);
         
+        searchReturnEl.textContent = '';
+        cityInputEl.value = '';
+        /*stateInputEl.value = '';
+        countryInputEl.value = ''; */  
+    } else {
+        alert('Please enter a City');
     }
 }
 
+var buttonClickHandeler = function (event){
+    var pastSearches = event.target.getAttribute('cityState');
+
+    if (pastSearches) {
+        getPastSearch(pastSearches);
+
+        searchReturnEl.textContent = '';
+    }
+}
+
+var newSearch = function (city) {
+
+    fetch(queryURL)
+        .then(response => {
+            console.log(response);
+        })
+        .then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayReturn(data, city);
+            })
+        } else {
+            alert('Error ' + response.statusText);
+        }
+    })
+}
+
+var getPastSearch = function (pastSearches) {
+    fetch(queryURL).then(response => {
+        console.log(response)
+    })
+    .then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayReturn(data.items, pastSearches);
+            })
+        } else {
+            alert('Error' + response.statusText);
+        }
+    })
+}
+
+var displayReturn = function (citySearch) {
+    if(response.ok) {
+       response.json().then(function (data) {
+        displayReturn(data.items, citySearch)
+       })
+        return;
+    }
+
+searchReturnEl.textContent = citySearch;
+
+for (var i = 0; i < response.lenth; i++){
+    var cityName = document.createElement('div');
+    cityN
+ }
+}
+searchInfoEl.addEventListener('submit', formSubmitHandler);
+pastSearchesEl.addEventListener('click', buttonClickHandeler);
 
